77de03b6d24193d2fe9c29799eb715eca868fa7d
Ku Jui <npukujui11@gmail.com>
2024-09-17 00:42:28 +0800

    实现跳表类中的构造函数和析构函数，成员方法get_random_level, create_node，insert_element, select_element

--- skiplist.h
+++ skiplist.h
@@ -157,3 +157,204 @@ private:
 
     bool is_valid_string(const std::string& str); // 判断字符串是否为有效字符串
 };
+
+/**
+ * 构造函数
+ * @param max_level 跳表的最大层数
+ * @return void 
+ */
+
+template <typename K, typename V>
+SkipList<K, V>::SkipList(int max_level) {
+    this->_max_level = max_level;
+    this->_skip_list_level = 0;
+    this->_element_count = 0;
+
+    // 创建头节点
+    K k;
+    V v;
+    this->_header = create_node(k, v, _max_level);
+}
+
+template <typename K, typename V>
+SkipList<K, V>::~SkipList() {
+    
+    if (_file_reader.is_open()) { // 关闭文件读取流
+        _file_reader.close();
+    }
+    if (_file_writer.is_open()) { // 关闭文件写入流
+        _file_writer.close();
+    }
+
+    // 清空跳表
+    if (_header->forward[0] != nullptr) { 
+        clear(_header->forward[0]);
+    }
+    delete _header; // 释放头节点的空间
+}
+
+template <typename K, typename V>
+int SkipList<K, V>::get_random_level() {
+    int k = 1; // 初始化层级，每个节点至少出现在第一层
+
+    while (rand() % 2) { // 生成随机数，如果是奇数，则层级+1
+        k++;
+    }
+    // 返回层级，但不能超过最大层级
+    return (k < _max_level) ? k : _max_level;
+}
+
+/**
+ * 创建节点
+ * @param k 节点的键
+ * @param v 节点的值
+ * @param level 节点的层数
+ * @return Node<K, V>* 返回创建的节点
+ */
+template <typename K, typename V>
+Node<K, V>* SkipList<K, V>::create_node(const K key, const V value, const int level) { 
+    Node<K, V>* n = new Node<K, V>(key, value, level); // 创建节点
+    return n;
+}
+
+// Insert given key and value in skip list 
+// return 1 means element exists  
+// return 0 means insert successfully
+/* 
+                           +------------+
+                           |  insert 50 |
+                           +------------+
+level 4     +-->1+                                                      100
+                 |
+                 |                      insert +----+
+level 3         1+-------->10+---------------> | 50 |          70       100
+                                               |    |
+                                               |    |
+level 2         1          10         30       | 50 |          70       100
+                                               |    |
+                                               |    |
+level 1         1    4     10         30       | 50 |          70       100
+                                               |    |
+                                               |    |
+level 0         1    4   9 10         30   40  | 50 |  60      70       100
+                                               +----+
+
+*/
+
+/**
+ * 插入元素
+ * @param key 要插入的元素的键
+ * @param value 要插入的元素的值
+ * @return 如果元素已存在，返回 1；否则，进行更新 value 操作并返回 0。
+ * @description 插入元素的过程是：
+ *                  1. 确定节点层级；
+ *                  2. 从头节点开始，查找每一层的节点，找到插入位置；
+ *                  3. 更新每一层的节点的指针。
+*/
+template <typename K, typename V>
+int SkipList<K, V>::insert_element(const K key, const V value) {
+
+    mtx.lock(); 
+    Node<K, V>* current = this->_header; // 从头节点开始
+
+    Node<K, V>* update[_max_level + 1]; // 用于记录每一层中待更新指针的节点
+    memset(update, 0, sizeof(Node<K, V>*) * (_max_level + 1)); // 初始化 update 数组
+
+    // 从最大层级开始，逐层查找节点
+    for (int i = _skip_list_level; i >= 0; i--) { 
+        while (current->forward[i] != nullptr && current->forward[i]->get_key() < key) {
+            current = current->forward[i];
+        }
+        // 记录每一层中待更新指针的节点
+        update[i] = current; 
+    }
+
+    // 移动到最底层的下一个节点，准备插入操作
+    current = current->forward[0];
+    // 检查待插入节点的键是否已经存在
+    if (current != nullptr && current->get_key() == key) { 
+        /*
+         * 这里可以考虑插入覆盖操作
+         * current->set_value(value);
+         */
+        std::cout << "Element with key " << key << " already exists." << std::endl;
+        mtx.unlock(); 
+        return 1; // 元素已存在
+    } else { // 键不存在或者当前节点为空
+    // current == nullptr || current->get_key() != key
+        int random_level = get_random_level(); // 生成随机层级
+        // 如果随机层级大于当前跳表的层级，则更新 update 数组
+        if (random_level > _skip_list_level) { 
+            // 对所有新的更高层级，将头节点设置为它们的前驱节点
+            for (int i = _skip_list_level + 1; i < random_level + 1; i++) { 
+                update[i] = _header;
+            }
+            _skip_list_level = random_level; // 更新跳表的层级
+        } 
+
+        // 创建新节点
+        Node<K, V>* inserted_node = create_node(key, value, random_level);
+        // 更新每一层的节点的指针
+        for (int i = 0; i <= random_level; i++) { 
+            // 新节点指向当前节点的下一个节点
+            inserted_node->forward[i] = update[i]->forward[i];
+            // 当前节点指向新节点
+            update[i]->forward[i] = inserted_node;
+        }
+        std::cout << "Element with key " << key << " inserted successfully." << std::endl;
+        _element_count++; // 更新元素计数
+    }
+    mtx.unlock();
+    return 0; // 插入成功
+}
+
+// Search for element in skip list 
+/*
+                           +------------+
+                           |  select 60 |
+                           +------------+
+level 4     +-->1+                                                      100
+                 |
+                 |
+level 3         1+-------->10+------------------>50+           70       100
+                                                   |
+                                                   |
+level 2         1          10         30         50|           70       100
+                                                   |
+                                                   |
+level 1         1    4     10         30         50|           70       100
+                                                   |
+                                                   |
+level 0         1    4   9 10         30   40    50+-->60      70       100
+*/
+
+/**
+ * 查找元素
+ * @param key 要查找的元素的键
+ * @return bool 如果找到返回true，否则返回false
+*/
+template <typename K, typename V>
+bool SkipList<K, V>::search_element(K key) {
+
+    std::cout << "search_element-----------------" << std::endl;
+    // 定义一个指针 current，初始化为跳表的头节点 _header
+    Node<K, V>* current = _header;
+
+    for (int i = _skip_list_level; i >= 0; i--) { // 从跳表的最高层开始查找
+        // 遍历当前层级，直到下一个节点的键值大于要查找的键值
+        while (current->forward[i] != nullptr && current->forward[i]->get_key() < key) {
+            // 移动到下一个节点
+            current = current->forward[i];
+        }
+        // 当前节点的下一个节点的键值大于待查找的键值时，进行下层操作
+    }
+    // 检查当前层（最底层）的下一个节点的键值是否为要查找的键值
+    current = current->forward[0];
+    if (current != nullptr && current->get_key() == key) { 
+        std::cout << "Found key: " << key << ", value: " << current->get_value() << std::endl;
+        return true; // 找到了
+    }
+
+    std::cout << " Not found key: " << key << std::endl;
+    return false; // 没找到
+}
